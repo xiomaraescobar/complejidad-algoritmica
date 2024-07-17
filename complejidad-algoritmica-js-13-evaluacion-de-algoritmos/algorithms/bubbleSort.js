7cacb480a22496e674f4dba81713131a93d990c3
Your Name <you@example.com>
2024-07-17 17:38:45 -0500

    complejidad temporal

---
+++ complejidad-algoritmica-js-13-evaluacion-de-algoritmos/algorithms/bubbleSort.js
@@ -0,0 +1,17 @@
+/**
+ * Complejidad Temporal -> O( n^2 )
+ * Complejidad Espacial -> O(  )
+ */
+function bubbleSort(arreglo) {
+  let longitud = arreglo.length; //O(1)
+  for (let i = 0; i < longitud; i++) {//O(n)
+    for (let j = 0; j < longitud; j++) { //O(n)
+      if (arreglo[j] > arreglo[j + 1]) { //O(1)
+        let temporal = arreglo[j];  //O(1)
+        arreglo[j] = arreglo[j + 1]; //O(1)
+        arreglo[j + 1] = temporal; //O(1)
+      }
+    }
+  }
+  return arreglo; //O(1)
+}
