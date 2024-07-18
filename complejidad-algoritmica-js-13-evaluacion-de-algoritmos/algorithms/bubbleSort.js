95792ba19d6ffaf9c9a6ee3270c18a3f0df8d923
Your Name <you@example.com>
2024-07-18 10:52:19 -0500

    complejidad espacial

--- complejidad-algoritmica-js-13-evaluacion-de-algoritmos/algorithms/bubbleSort.js
+++ complejidad-algoritmica-js-13-evaluacion-de-algoritmos/algorithms/bubbleSort.js
@@ -1,17 +1,18 @@
 /**
  * Complejidad Temporal -> O( n^2 )
- * Complejidad Espacial -> O(  )
+ * Complejidad Espacial -> O( n )
+ * * Espacio Auxiliar-> O(1)
  */
-function bubbleSort(arreglo) {
+function bubbleSort(arreglo) { //O(n)
   let longitud = arreglo.length; //O(1)
-  for (let i = 0; i < longitud; i++) {//O(n)
-    for (let j = 0; j < longitud; j++) { //O(n)
-      if (arreglo[j] > arreglo[j + 1]) { //O(1)
+  for (let i = 0; i < longitud; i++) {//O(1)
+    for (let j = 0; j < longitud; j++) { //O(1)
+      if (arreglo[j] > arreglo[j + 1]) { 
         let temporal = arreglo[j];  //O(1)
-        arreglo[j] = arreglo[j + 1]; //O(1)
-        arreglo[j + 1] = temporal; //O(1)
+        arreglo[j] = arreglo[j + 1]; 
+        arreglo[j + 1] = temporal; 
       }
     }
   }
-  return arreglo; //O(1)
+  return arreglo; 
 }
--- complejidad-algoritmica-js-13-evaluacion-de-algoritmos/algorithms/linearSearch.js
+++ complejidad-algoritmica-js-13-evaluacion-de-algoritmos/algorithms/linearSearch.js
@@ -1,13 +1,13 @@
 /**
  * Complejidad Temporal -> O( n )
- * Complejidad Espacial -> O(  )
- * Espacio Auxiliar -> O(  )
+ * Complejidad Espacial -> O( n  )
+ * Espacio Auxiliar = compejidad espacial - espacio de entrada O(n) - O(1) para un resultado de -> O( 1 )
  */
-function linearSearch(arreglo, clave) {  
-  for (let indice = 0; indice < arreglo.length; indice++) { //O(n)
-    if (arreglo[indice] === clave) { //O(1)
-      return indice; //O(1)
+function linearSearch(arreglo, clave) {   //O(n)
+  for (let indice = 0; indice < arreglo.length; indice++) { //O(1)
+    if (arreglo[indice] === clave) { 
+      return indice; 
     }
   }
-  return -1; //(1)
+  return -1; 
 }
